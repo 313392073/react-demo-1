dad0fb09d60e75eafda60d664992ef8e11e196f4
lishiwenjing <1071186443@qq.com>
2017-06-29 18:51:50 +0800

    new project

---
+++ my-app/src/component/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+import App2 from './component/App2';
+
+class App extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			time: 0
+		}
+	}
+
+	tick(){
+		let newTime = this.state.time + 1;
+		this.setState({time: newTime});
+	}
+
+	componentDidMount(){
+		setInterval(() => this.tick(),1000);
+	}
+
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo"/>
+					<h2>Welcome to React</h2>
+				</div>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+				<div>opened seconds: {this.state.time}</div>
+				<App2/>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ my-app/src/component/App2.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+import React, {Component} from 'react';
+import '../css/App2.css';
+
+class App2 extends Component{
+	render() {
+		return (
+			<div className="App2">
+				<div className="App2-header">
+					<h2>App2 header</h2>
+				</div>
+				<div className="App2-content">
+					App2 content
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App2;
