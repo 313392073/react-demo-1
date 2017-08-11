24c0cb486ed7d2e0ed4d220421def0903dae8f92
lishiwenjing <1071186443@qq.com>
2017-08-11 19:01:16 +0800

    add redux-logger

---
+++ README.md
@@ -0,0 +1,2 @@
+### redux
+1. redux 处理异步操作可使用redux-thunk中间件或者redux-promise
--- my-app5/src/store.js
+++ my-app5/src/store.js
@@ -2,7 +2,8 @@
  * Created by Administrator on 2017/7/25.
  */
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
+import logger from 'redux-logger';
 
-export default createStore(reducers);
+export default createStore(reducers,applyMiddleware(logger));
